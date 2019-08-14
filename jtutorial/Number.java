30246d648da9ba1b2696f913f1154863155c99ee
egoing <egoing@gmail.com>
2019-08-14 16:16:22 +0900

    commit

--- jtutorial/Number.java
+++ jtutorial/Number.java
@@ -4,7 +4,7 @@ public class Number {
 
 	public static void main(String[] args) {
 		Integer left = 1;
-		Integer right = 3;
+		Integer right = 3.2;
 		System.out.println(left+right);
 		System.out.println(left.compareTo(right));
 		
