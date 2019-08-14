1778c0d1a936aa311085982fc6167acab81b1779
egoing <egoing@gmail.com>
2019-08-14 10:39:06 +0900

    commit

--- jtutorial/Number.java
+++ jtutorial/Number.java
@@ -3,10 +3,14 @@ package jtutorial;
 public class Number {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Integer left = 1;
 		Integer right = 1;
 		System.out.println(left+right);
+		
+		String name = "egoing";
+		String letter = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend diam ac iaculis placerat. Nulla iaculis porta erat ut fringilla. Nulla viverra molestie arcu et laoreet. Nam vel lectus euismod, bibendum odio eu, interdum ante. Nunc fermentum arcu id quam mollis, ac tincidunt diam ultricies. Donec sollicitudin eu odio non tincidunt. Etiam bibendum massa sed elit posuere feugiat. Sed quis lacus faucibus dui viverra varius in sit amet leo. Vivamus nec eros arcu. Nunc tristique lacinia sapien eu blandit. Integer vitae ullamcorper nisi. Aliquam tempor justo non mi tristique pellentesque. Nam consequat iaculis est, nec lobortis eros euismod eget. Morbi facilisis dolor arcu, nec hendrerit elit mollis a. Nullam mattis interdum molestie. Sed eu sapien accumsan, faucibus felis quis, commodo enim.\n" + 
+				"\n" + 
+				"Vivamus blandit, nisi eget dictum pharetra, orci dui placerat velit, sed aliquam eros odio ut risus. Phasellus metus augue, faucibus at pulvinar sed, ultrices non justo. Nam maximus, risus sit amet convallis tempus, lectus nisi lobortis neque, nec pharetra est metus quis neque. Fusce pretium odio nec porttitor consectetur. Sed imperdiet consectetur magna, sit amet aliquet eros aliquam ac. Integer tellus lectus, scelerisque quis ullamcorper quis, ornare ornare justo. Integer bibendum consequat dui at cursus. Aenean interdum euismod tempus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras justo lorem, tristique non lorem a, fermentum hendrerit odio. Ut lobortis facilisis nisl a ornare. Phasellus imperdiet mauris felis, quis dapibus mauris suscipit vitae. Duis venenatis mauris id elit tristique maximus. Aliquam vel sollicitudin magna. Sed facilisis ipsum sapien, sit amet porta ante dictum eget. Integer commodo urna id sapien eleifend vehicula.";
 	}
 
 }
